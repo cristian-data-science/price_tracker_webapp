d24cd771b451bf995f0e7bc2f7081a841965e770
root <root@ip-172-31-37-187.sa-east-1.compute.internal>
2022-03-02 02:20:36 +0000

    New store add

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ Esta app consume los datos de precio y stock de un webscaper automatizado (rastr
 progress_bar = st.sidebar.progress(0)
 status_text = st.sidebar.empty()
 last_rows = pd.DataFrame(
-     np.random.rand(1, 3),
+     np.random.rand(1, 4),
      columns=['PcFactory', 'SPDigital', 'Winpy','TecnoMaster'])
 
 chart = st.line_chart(last_rows)
 
 for i in range(1, 101):
-    new_rows = last_rows + np.random.randn(1, 3).cumsum(axis=0)
+    new_rows = last_rows + np.random.randn(1, 4).cumsum(axis=0)
     status_text.text("%i%% Completado" % i)
     chart.add_rows(new_rows)
     progress_bar.progress(i)
